eae964f5056ec0f6088701980176ffa0a74403b9
Siddharth Mudgal <siddharthmudgal@Siddharths-MacBook-Pro-2.local>
2021-05-09 02:01:23 +0800

    FLAKE cleaning

--- app/event/views.py
+++ app/event/views.py
@@ -3,7 +3,7 @@ from rest_framework.decorators import action
 from rest_framework.response import Response
 
 from core.models import Event
-from .serializers import EventSerializer, EventSearchSerializer
+from .serializers import EventSerializer
 
 from rest_framework import viewsets
 from .service import selectively_find_events
